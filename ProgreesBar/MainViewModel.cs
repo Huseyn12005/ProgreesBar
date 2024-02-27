7b592e2be86efe82a992ccc5323908a65f2b4715
Huseyn <horucov148@gmail.com>
2024-02-27 17:18:30 +0400

    Add project files.

---
+++ ProgreesBar/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ProgreesBar.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ProgreesBar
+{
+    class MainViewModel
+    {
+       
+    }
+}
